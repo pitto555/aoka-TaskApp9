b579089eddcf0e2a1649baabbec5e9f6f367d386
aoka <aoka.555.ibiza@gmail.com>
2021-04-07 20:36:07 +0900

    Firast Commit

--- aoka-TaskApp9/ViewController.swift
+++ aoka-TaskApp9/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var textLabel: UILabel!
+    let prefecturesArray = ["東京都","神奈川県","埼玉県","千葉県"]
+    
+    @IBAction func exitCansel(segue:UIStoryboardSegue){
     }
-
+    
+    @IBAction func exitTokyo(segue:UIStoryboardSegue) {
+        textLabel.text = prefecturesArray[0]
+    }
+    
+    @IBAction func exitKanagawa(segue:UIStoryboardSegue) {
+        textLabel.text = prefecturesArray[1]
+    }
+    
+    @IBAction func exitSaitama(segue:UIStoryboardSegue) {
+        textLabel.text = prefecturesArray[2]
+    }
+    
+    @IBAction func exitTiba(segue:UIStoryboardSegue) {
+        textLabel.text = prefecturesArray[3]
+    }
 
 }
 
